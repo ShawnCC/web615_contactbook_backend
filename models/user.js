7c3ad09f1b1760fb110236bee9e904b4407c3f41
Shawn Crusvar <scrusvar@alterisgroup.net>
2017-01-21 20:00:28 -0500

    - Week 3 code - authentication via JWTs, updates to contact endpoints to compare IDs of user to ID stored in JWT

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
+// Number of rounds the password is run through a hashing algorithm. The more rounds the more secure, but also the more time consuming
+const saltRounds = 10;
+
 module.exports = (sequelize, DataTypes) => {
     var User = sequelize.define('user', {
             name: {
@@ -31,6 +35,31 @@ module.exports = (sequelize, DataTypes) => {
                 User.hasMany(models.contact, {
                     onDelete: 'CASCADE'
                 });
+            }
+        },
+
+        instanceMethods: {
+            /**
+             * Hashes a plaintext password
+             * @param {String} password - The plaintext password to hash
+             */
+            hashPassword: (password) => {
+                return bcrypt.hashSync(password, saltRounds);
+            },
+
+            /**
+             * Verifys a plaintext password against that hash stored in the model
+             * @param {String} password - The plaintext password to verify
+             * @param {String} hash - The hashed password to verify against
+             */
+            verifyPassword: (password, hash) => {
+                return bcrypt.compareSync(password, hash);
+            }
+        },
+
+        hooks: {
+            beforeCreate: (instance) => {
+                instance.password = bcrypt.hashSync(instance.password, saltRounds);
             }
         }
     });
--- routes/api/v1/contacts.js
+++ routes/api/v1/contacts.js
@@ -4,12 +4,13 @@ const express = require('express');
 const router = express.Router();
 const async = require('async');
 const models = require('../../../models');
+const authUtils = require('../../../utils/auth');
 
 /**
  * Get all the contacts
  * GET /api/v1/contacts/
  */
-router.get('/', (req, res) => {
+router.get('/', authUtils.jwtAuth, (req, res) => {
     let responseData = {};
 
     models.contact.findAll({
@@ -39,25 +40,59 @@ router.get('/', (req, res) => {
  * Get a contact
  * GET /api/v1/contacts/:contactId/
  */
-router.get('/:contactId/', (req, res) => {
+router.get('/:contactId/', authUtils.jwtAuth, (req, res) => {
     let responseData = {};
+    let contact;
 
-    models.contact.findById(req.params.contactId).then((data) => {
-        responseData.status = 200;
-        responseData.message = 'Contact retrieved successfully!';
-        responseData.contact = data;
+    async.series([
+        // Get the contact, and store it in the contact variable
+        (callback) => {
+            models.contact.findById(req.params.contactId).then((data) => {
+                if (!data) {
+                    responseData.status = 404;
+                    responseData.message = 'Contact not found';
 
-        res.status(responseData.status);
-        res.json(responseData);
-    }).catch((err) => {
-        console.log(new Date());
-        console.log(err);
+                    return callback(new Error(responseData.message));
+                }
 
-        responseData.status = 500,
-        responseData.message = 'Error getting Contact.';
+                contact = data.get({ plain: true });
+                return callback();
+            }).catch((err) => {
+                responseData.status = 500,
+                responseData.message = 'Error getting Contact.';
 
-        res.status(responseData.status);
-        res.json(responseData);
+                return callback(err);
+            });
+        },
+
+        // Verify that the ID of the user in req.decoded, and the user_id in the
+        // contact match
+        (callback) => {
+            // Compare the IDs, if they're not equal return with an error
+            if (req.decoded.id !== contact.user_id) {
+                responseData.status = 403;
+                responseData.message = 'You have insufficient permissions to access the Contact.';
+
+                return callback(new Error(responseData.message));
+            }
+
+            return callback();
+        }
+    ], (err) => {
+        if (err) {
+            console.log(new Date());
+            console.log(err);
+            
+            res.status(responseData.status);
+            res.json(responseData);
+        } else {
+            responseData.status = 200;
+            responseData.message = 'Contact retrieved successfully!';
+            responseData.contact = contact;
+
+            res.status(responseData.status);
+            res.json(responseData);
+        }
     });
 });
 
@@ -65,25 +100,50 @@ router.get('/:contactId/', (req, res) => {
  * Create a contact
  * POST /api/v1/contacts/
  */
-router.post('/', (req, res) => {
+router.post('/', authUtils.jwtAuth, (req, res) => {
     let responseData = {};
 
-    models.contact.create(req.body.contact).then((data) => {
-        responseData.status = 201,
-        responseData.message = 'Contact created successfully!';
-        responseData.contact = data;
+    async.series([
+        // Verify that the ID of the user in req.decoded, and the user_id in the
+        // request body match
+        (callback) => {
+            if (req.decoded.id !== req.body.contact.user_id) {
+                responseData.status = 403;
+                responseData.message = 'You have insufficient permissions to access the Contact.';
 
-        res.status(responseData.status);
-        res.json(responseData);
-    }).catch((err) => {
-        console.log(new Date());
-        console.log(err);
+                return callback(new Error(responseData.message));
+            }
 
-        responseData.status = 500,
-        responseData.message = 'Error creating Contact.';
+            return callback();
+        },
 
-        res.status(responseData.status);
-        res.json(responseData);
+        // Create the Contact
+        (callback) => {
+            models.contact.create(req.body.contact).then((data) => {
+                responseData.contact = data;
+
+                return callback();
+            }).catch((err) => {
+                responseData.status = 500,
+                responseData.message = 'Error creating Contact.';
+
+                return callback(err);
+            });
+        }
+    ], (err) => {
+        if (err) {
+            console.log(new Date());
+            console.log(err);
+            
+            res.status(responseData.status);
+            res.json(responseData);
+        } else {
+            responseData.status = 201;
+            responseData.message = 'Contact created successfully!';
+
+            res.status(responseData.status);
+            res.json(responseData);
+        }
     });
 });
 
@@ -91,7 +151,7 @@ router.post('/', (req, res) => {
  * Update a contact
  * PUT /api/v1/contacts/:contactId/
  */
-router.put('/:contactId/', (req, res) => {
+router.put('/:contactId/', authUtils.jwtAuth, (req, res) => {
     let responseData = {};
     let contact;
 
@@ -113,6 +173,20 @@ router.put('/:contactId/', (req, res) => {
 
                 return callback(err);
             });
+        },
+
+        // Verify that the ID of the user in req.decoded, and the user_id in the
+        // contact match
+        (callback) => {
+            // Compare the IDs, if they're not equal return with an error
+            if (req.decoded.id !== contact.user_id) {
+                responseData.status = 403;
+                responseData.message = 'You have insufficient permissions to access the Contact.';
+
+                return callback(new Error(responseData.message));
+            }
+
+            return callback();
         },
 
         // Update and save the contact
@@ -153,28 +227,73 @@ router.put('/:contactId/', (req, res) => {
  * Delete a contact
  * DELETE /api/v1/contacts/:contactId/
  */
-router.delete('/:contactId/', (req, res) => {
+router.delete('/:contactId/', authUtils.jwtAuth, (req, res) => {
     let responseData = {};
+    let contact;
 
-    models.contact.destroy({
-        where: {
-            id: req.params.contactId
+    async.series([
+        // Get the contact
+        (callback) => {
+            models.contact.findById(req.params.contactId).then((data) => {
+                if (data) {
+                    contact = data;
+                    return callback();
+                } else {
+                    responseData.status = 404,
+                    responseData.message = 'Contact not found.';
+                    return callback(new Error(responseData.message));
+                }
+            }).catch((err) => {
+                responseData.status = 500,
+                responseData.message = 'Error getting Contact.';
+
+                return callback(err);
+            });
+        },
+
+        // Verify that the ID of the user in req.decoded, and the user_id in the
+        // contact match
+        (callback) => {
+            // Compare the IDs, if they're not equal return with an error
+            if (req.decoded.id !== contact.user_id) {
+                responseData.status = 403;
+                responseData.message = 'You have insufficient permissions to access the Contact.';
+
+                return callback(new Error(responseData.message));
+            }
+
+            return callback();
+        },
+
+        // Delete the contact
+        (callback) => {
+            models.contact.destroy({
+                where: {
+                    id: req.params.contactId
+                }
+            }).then(() => {
+                return callback();
+            }).catch((err) => {
+                responseData.status = 500,
+                responseData.message = 'Error deleting Contact.';
+
+                return callback(err);
+            });
         }
-    }).then(() => {
-        responseData.status = 200,
-        responseData.message = 'Contact deleted successfully!';
+    ], (err) => {
+        if (err) {
+            console.log(new Date());
+            console.log(err);
 
-        res.status(responseData.status);
-        res.json(responseData);
-    }).catch((err) => {
-        console.log(new Date());
-        console.log(err);
+            res.status(responseData.status);
+            res.json(responseData);
+        } else {
+            responseData.status = 200;
+            responseData.message = 'Contact deleted successfully!';
 
-        responseData.status = 500,
-        responseData.message = 'Error deleting Contact.';
-
-        res.status(responseData.status);
-        res.json(responseData);
+            res.status(responseData.status);
+            res.json(responseData);
+        }
     });
 });
 
