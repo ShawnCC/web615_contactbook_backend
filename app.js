d47c4fc86a3ba940aa54e4e953456a6c6a71343d
Shawn Crusvar <scrusvar@alterisgroup.net>
2017-03-18 18:55:09 -0400

    - Updating function syntax

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
